1bdf1a0627c7b065290695b700b4b0332ff308ee
Laszlo Dobos <dobos@complex.elte.hu>
2016-06-23 23:06:08 +0200

    Notebook image updated. Now works with LDAP and jupyter is started.

--- modules/notebook/configure.sh
+++ modules/notebook/configure.sh
@@ -17,16 +17,34 @@ case $VERB in
     cp etc/nslcd.conf $SRV/notebook/etc
     
     mkdir -p $SRV/notebook/init
-    echo "mount -t nfs $PROJECT-home:/exports/home /home" >> $SRV/notebook/init/0.sh
-        
+    # NFS mount for home
+    echo "#/bin/sh
+echo \"Mounting home...\"
+mount -t nfs $PROJECT-home:/exports/home /home" \
+      > $SRV/notebook/init/0.sh
+    # Start jupyter
+    echo "#/bin/sh
+echo \"Starting notebook for \$NB_USER...\"
+cd /home/\$NB_USER
+. start-notebook.sh --NotebookApp.base_url=\$NB_URL --NotebookApp.port=\$NB_PORT " \
+      > $SRV/notebook/init/1.sh
+    
+    # TODO: we create a notebook container here for testing but
+    # individual containers will later be created for single
+    # users
     docker $DOCKERARGS create \
       --name $PROJECT-notebook \
       --hostname $PROJECT-notebook \
       --net $PROJECT-net \
-      --ip $OWNCLOUDIP \
+      --ip $NOTEBOOKIP \
       --privileged \
       -v $SRV/notebook/etc/nslcd.conf:/etc/nslcd.conf \
       -v $SRV/notebook/init:/init \
+      -e NB_USER=test \
+      -e NB_UID=10002 \
+      -e NB_GID=10002 \
+      -e NB_URL=/notebook/test/ \
+      -e NB_PORT=8888 \
       kooplex-notebook
     
     rmetc
--- modules/notebook/start-notebook.sh
+++ modules/notebook/start-notebook.sh
@@ -2,6 +2,7 @@
 
 # Handle special flags if we're root
 if [ $UID == 0 ] ; then
+
     # Change UID of NB_USER to NB_UID if it does not match
     if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
         usermod -u $NB_UID $NB_USER
