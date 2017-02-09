fae47a2287938eb874f09000479c2ed21a46c851
David Visontai <jegesm@gmail.com>
2017-02-09 14:00:29 +0100

    new image with git

---
+++ modules/notebook/image-numpy-git/scripts/start-notebook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Handle special flags if we're root
+if [ $UID == 0 ] ; then
+
+	echo " 1"
+    # Change UID of NB_USER to NB_UID if it does not match
+    if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
+        usermod -u $NB_UID $NB_USER
+        echo " 12"
+#        chown -R $NB_UID $CONDA_DIR
+        echo " 13"
+    fi
+	echo " 2"
+    # Enable sudo if requested
+    if [ ! -z "$GRANT_SUDO" ]; then
+        echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook
+    fi
+	echo " 3"
+    # Start the notebook server
+    exec su $NB_USER -c "env PATH=$PATH jupyter notebook $*"
+    echo " masik 4"
+else
+    # Otherwise just exec the notebook
+    exec jupyter notebook $*
+    echo " masik 1"
+fi
+
