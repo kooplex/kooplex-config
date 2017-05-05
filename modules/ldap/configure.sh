c1a6cb5dc49674b81d4c94fc2ee86de754b3df1f
jegesm <jegesm@gmail.com>
2017-05-05 20:00:35 +0200

    There was some hickup in the ldap setup. Maybe now it is ok, Fix 2

--- modules/ldap/configure.sh
+++ modules/ldap/configure.sh
@@ -25,7 +25,7 @@ case $VERB in
       -v $SRV/ldap/var:/var/lib/ldap \
       -e SLAPD_PASSWORD="$LDAPPASS" \
       -e SLAPD_CONFIG_PASSWORD="$LDAPPASS" \
-      -e SLAPD_DOMAIN=$LDAPORG \
+      -e SLAPD_DOMAIN=$LDAPDOMAIN \
       kooplex-ldap 
       
 
