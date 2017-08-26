6bade18e64afdc94993c40e6a1d81382f277c092
Steger, Jozsef <steger@complex.elte.hu>
2017-08-26 06:13:43 +0200

    make it possible to sync if a timer expires periodically to make sure remote changes are synced

--- modules/occ/wd.py
+++ modules/occ/wd.py
@@ -2,9 +2,10 @@
 """
 Author: Jozsef Steger
 Created: 23. May 2017.
-Summary: watch a filesystem directory recursively for changes, 
-         and when they happen with a limited rate issue synchronization 
-         to the owncloud.
+Change: 26. Aug 2017.
+Summary: synchronize a given filesystem directory to the owncloud.
+         Synchronization is triggered by filesystem changes or when a timer is periodically expired.
+         Synchronization is delayed if it should occur with a higher than the limited rate.
 """
 import time
 from watchdog.observers import Observer
@@ -18,12 +19,32 @@ class MySync():
     synced = threading.Event()
     alert = threading.Event()
 
-    def __init__(self, command, syncwait = 10, stoppath = '.', debug = False):
+    def __init__(self, command, syncperiod = 10, syncwait = 10, stoppath = '.', debug = False):
         self.enabled.set()
         self.command = command
+        self.syncperiod = syncperiod
         self.syncwait = syncwait
         self.stoppath = stoppath
         self.debug = debug
+        self._timer = None
+        self.start_periodictimer()
+
+    def start_periodictimer(self):
+        self.stop_periodictimer()
+        self._timer = threading.Timer(self.syncperiod, self.expire_periodictimer)
+        self._timer.start()
+
+    def stop_periodictimer(self):
+        if self._timer:
+            self._timer.cancel()
+            self._timer = None
+
+    def expire_periodictimer(self):
+        if self.debug:
+            print ("waited %d time to sync with with owncloud" % (self.syncperiod))
+        self.synced.clear()
+        self.start_periodictimer()
+        self.commit()
 
     def register(self, e):
         if self.debug:
@@ -35,7 +56,7 @@ class MySync():
                 print ("%s removed, I won't sync to owncloud to avid dataloss" % self.stoppath)
         self.commit()
 
-    def expired(self):
+    def expire_choketimer(self):
         if self.debug:
             print ("expired")
         self.enabled.set()
@@ -50,7 +71,7 @@ class MySync():
             print ("committing")
         subprocess.call(self.command)
         self.synced.set()
-        t = threading.Timer(self.syncwait, self.expired)
+        t = threading.Timer(self.syncwait, self.expire_choketimer)
         t.start()
 
 class MyHandler(RegexMatchingEventHandler):
@@ -77,6 +98,7 @@ if __name__ == '__main__':
     parser.add_argument("-d", "--debug", action = "store_true", dest = "debug", help = "whether to show what is going on")
     parser.add_argument("-S", "--verbose", action = "store_true", dest = "debugoc", help = "whether to show what is going on with the owncloudcmd")
     parser.add_argument("-c", "--cloud-url", action = "store", dest = "url", default = "https://compare.vo.elte.hu/owncloud", help = "owncloud API url")
+    parser.add_argument("-p", "--sync-period", action = "store", type = int, dest = "syncperiod", default = 10, help = "synchronize this often even if no local changes so as to get synced with owncloud")
     parser.add_argument("-w", "--sync-wait", action = "store", type = int, dest = "syncwait", default = 10, help = "wait some seconds between syncs to prevent the cloud API from flooding")
     parser.add_argument("-R", "--avoid-recursive", action = "store_false", dest = "recursive", help = "whether to avoid recursice check on folder")
     parser.add_argument("-i", "--ignore-pattern", nargs = "*", action = "store", dest = "ignore", help = "regular expressions of files to ignore", default = [r"^\.", r"^.*/\."])
@@ -92,7 +114,7 @@ if __name__ == '__main__':
         command.insert(1, '-s')
     if args.debug:
         print ("sync command: %s" % command)
-    sync = MySync(command, syncwait = args.syncwait, stoppath = path, debug = args.debug)
+    sync = MySync(command, syncperiod = args.syncperiod, syncwait = args.syncwait, stoppath = path, debug = args.debug)
     sync.commit()
     ignore_regexes = [ re.compile(r) for r in args.ignore ]
     observer = Observer()
