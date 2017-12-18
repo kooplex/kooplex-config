c65574c483865546489fb7b97cfc0b339585fac5
Steger, Jozsef <steger@complex.elte.hu>
2017-12-18 14:14:15 +0100

    typo

--- modules/impersonator/configure.sh
+++ modules/impersonator/configure.sh
@@ -25,7 +25,7 @@ case $VERB in
 #          -e "s/##LDAPBINDPW##/$LDAPPASS/" \
 #          -e "s/##LDAPBINDROOT##/cn=admin,$LDAPORG/" \
 #          -e "s/##LDAPBINDROOTPW##/$LDAPPASS/" etc/nslcd.conf_template > $RF/nslcd.conf
-      ldap_nslcd_config > $RF/nslcd.conf
+      ldap_nslcdconfig > $RF/nslcd.conf
 
       echo "2. Building ${PREFIX}-impersonator..."
       docker-compose $DOCKER_HOST -f $DOCKER_COMPOSE_FILE build 
