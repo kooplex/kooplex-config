c9358df1df2321fc60c240563113a1bb55896e8e
Steger, Jozsef <steger@complex.elte.hu>
2018-01-16 22:51:31 +0100

    user home at this stage does not exist

--- modules/notebook/scripts/1.sh
+++ modules/notebook/scripts/1.sh
@@ -2,7 +2,6 @@
 
 if [ -z "$REPORT" ] ; then 
     echo "Starting notebook for $NB_USER..."
-    cd /home/$NB_USER
     . start-notebook.sh --config=/etc/jupyter_notebook_config.py --log-level=DEBUG --NotebookApp.base_url=$NB_URL --NotebookApp.port=$NB_PORT --NotebookApp.token=$NB_TOKEN
 else
     echo "Starting Report Server"
