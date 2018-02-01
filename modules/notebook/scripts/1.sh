6c31c83c37ec66283e6b57efc958d80bc938c969
David Visontai <jegesm@gmail.com>
2018-02-01 13:11:59 +0100

    notebook and report fixes

--- modules/notebook/scripts/1.sh
+++ modules/notebook/scripts/1.sh
@@ -5,5 +5,5 @@ if [ -z "$REPORT_FILE" ] ; then
     . start-notebook.sh --config=/etc/jupyter_notebook_config.py --log-level=DEBUG --NotebookApp.base_url=$NB_URL --NotebookApp.port=$NB_PORT --NotebookApp.token=$NB_TOKEN
 else
     echo "Starting Report Server"
-    . start-report.sh --allow-root --config=/etc/jupyter_report_config.py --log-level=DEBUG --NotebookApp.base_url=$NB_URL --NotebookApp.port=$NB_PORT --NotebookApp.token=$NB_TOKEN
+    . start-report.sh --config=/etc/jupyter_report_config.py --log-level=DEBUG --NotebookApp.base_url=$NB_URL --NotebookApp.port=$NB_PORT --NotebookApp.token=$NB_TOKEN
 fi
