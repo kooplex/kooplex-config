4631f0aaa8a3b9f7757b81c7da4017e02cdbf7d9
David Visontai <jegesm@gmail.com>
2018-05-24 17:03:58 +0200

    read only user for monitordb

--- modules/monitor/scheme.sql
+++ modules/monitor/scheme.sql
@@ -7,15 +7,15 @@ create table hubuser (
   username varchar(64) not null
 );
 
+create table project (
+  id serial primary key,
+  project varchar(64) not null
+);
+
 create table container (
   id serial primary key, 
   project_id     int references project (id),
   container_name varchar(64) not null
-);
-
-create table project (
-  id serial primary key,
-  project varchar(64) not null
 );
 
 create table containerstats_current (
@@ -24,7 +24,7 @@ create table containerstats_current (
   container_id   int references container (id),
   last_read      timestamp not null default now(),
   cpuload        int not null, -- tizezrelek
-  memoryusage    int not null, -- byte
+  memoryusage    bigint not null, -- byte
   pids           int not null
 );
 
@@ -33,11 +33,24 @@ create table containerstats_aggregate (
   hubuser_id     int references hubuser (id),
   container_id   int references container (id),
   last_read      timestamp not null default now(),
-  net_i          int not null, -- byte
-  net_o          int not null, -- byte
-  block_i        int not null, -- byte
-  block_o        int not null  -- byte
+  net_i          bigint not null, -- byte
+  net_o          bigint not null, -- byte
+  block_i        bigint not null, -- byte
+  block_o        bigint not null  -- byte
 );
 
 
 -- TODO: host metrikák
+
+#CREATE ROLE readaccess;
+#GRANT USAGE ON SCHEMA public TO readaccess;
+
+CREATE USER usage_viewer WITH PASSWORD 'whatisusage';
+GRANT CONNECT ON DATABASE monitor to usage_viewer;
+\c monitor
+#GRANT readaccess TO usage_viewer;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO usage_viewer;
+GRANT USAGE ON SCHEMA public to usage_viewer;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO usage_viewer;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO usage_viewer;
+
