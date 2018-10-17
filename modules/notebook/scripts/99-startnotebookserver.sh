5cf3a8009677122c7781b10e04c4d05f72dcf1a3
jegesm <korte>
2018-10-17 15:36:18 +0200

    set nice levele 19 to notebook server

--- modules/notebook/scripts/99-startnotebookserver.sh
+++ modules/notebook/scripts/99-startnotebookserver.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$REPORT_FILE" ] ; then 
     echo "Starting notebook for $NB_USER..."
-    . start-notebook.sh --config=/etc/jupyter_notebook_config.py --log-level=DEBUG --NotebookApp.base_url=$NB_URL --NotebookApp.port=$NB_PORT --NotebookApp.token=$NB_TOKEN
+    nice -n 19 start-notebook.sh --config=/etc/jupyter_notebook_config.py --log-level=DEBUG --NotebookApp.base_url=$NB_URL --NotebookApp.port=$NB_PORT --NotebookApp.token=$NB_TOKEN
 else
     echo "Starting Report Server"
     . start-report.sh --allow-root --config=/etc/jupyter_report_config.py --log-level=DEBUG --NotebookApp.base_url=$NB_URL --NotebookApp.port=$NB_PORT --NotebookApp.token=$NB_TOKEN
