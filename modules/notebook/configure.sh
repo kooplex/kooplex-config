30bd333026fda6ca06ed29c9909ce04c7306f7ad
jegesm <jegesm@gmail.com>
2019-01-16 22:29:04 +0100

    added kernelgateway and bokeh server to containers (report); temporary rstudio login.html

--- modules/notebook/configure.sh
+++ modules/notebook/configure.sh
@@ -17,8 +17,8 @@ case $VERB in
         mkdir -p $RF/$imagedir
         sed -e "s/##PREFIX##/${PREFIX}/" $imagedir/Dockerfile-template > $RF/$imagedir/Dockerfile
         sed -e "s/##PREFIX##/${PREFIX}/" scripts/start-notebook.sh-template > $RF/$imagedir/start-notebook.sh
-        cp scripts/{kooplex-logo.png,jupyter_notebook_config.py,??-*.sh,manage_mount.sh,jupyter-notebook-kooplex} ${RF}/$imagedir
-        cp scripts/{entrypoint-rstudio.sh,bashrc_tail,rsession.conf,rserver.conf,rstudio-nginx.conf}  ${RF}/$imagedir
+        cp scripts/{kooplex-logo.png,jupyter_notebook_config.py,??-*.sh,manage_mount.sh,jupyter-notebook-kooplex,kernel-gateway.sh} ${RF}/$imagedir
+        cp scripts/{entrypoint-rstudio.sh,bashrc_tail,rsession.conf,rstudio-login.html,rserver.conf,rstudio-nginx.conf}  ${RF}/$imagedir
         cp DockerFile-pieces/* ${RF}/$imagedir
 
 #####
