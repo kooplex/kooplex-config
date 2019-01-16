5706f1971c3be2d9cfe5ead4e78b66b759d3b043
jegesm <jegesm@gmail.com>
2019-01-16 21:23:54 +0100

    fixed cron and stats.py for collecting monitordb

--- modules/monitor/scripts/stat.py
+++ modules/monitor/scripts/stat.py
@@ -1,6 +1,7 @@
 import docker
 import psycopg2
-
+from numpy import *
+import datetime
 
 def insert_item(table, col_name, value):
     sqlc = "INSERT INTO %s (%s) VALUES ('%s');" % (table, col_name, value)
@@ -9,7 +10,7 @@ def insert_item(table, col_name, value):
 
 def update_item(table, col_name, value, exist_col_name, exist_value):
     sqlc = "UPDATE %s SET %s = '%s' WHERE %s = '%s';" % (table, col_name, value, exist_col_name, exist_value)
-    print(sqlc)
+    #print(sqlc)
     cur.execute(sqlc)
     conn.commit()
 
@@ -30,9 +31,9 @@ def get_user_name(container):
         if "NB_USER" in i:
             return i.split('=')[1]
 
-def check_last_changed(table_name,  container_id, col_name, value, threshold, s_type):
-    sqlc = "select * from %s where container_id = %d \
-      order by last_read desc limit 1;" % (table_name, container_id)
+def check_last_changed(table_name, hubuser_id, container_id, col_name, value, threshold, s_type):
+    sqlc = "select * from %s where hubuser_id = %d and container_id = %d \
+      order by last_read desc limit 1;" % (table_name, hubuser_id, container_id)
     cur.execute(sqlc)
     res = cur.fetchone()
     if res:
@@ -49,19 +50,46 @@ def check_last_changed(table_name,  container_id, col_name, value, threshold, s_
 
     return False
 
-def insert_new_row(table_name, container_id, col_name, value):
-    sqlc = "INSERT INTO %s (container_id, %s) VALUES (%d, %s);" % \
-           (table_name, col_name, container_id, value)
+def insert_new_row(table_name, hubuser_id, container_id, col_name, value):
+    sqlc = "INSERT INTO %s (hubuser_id, container_id, %s) VALUES (%d, %d, %s);" % \
+           (table_name, col_name, hubuser_id, container_id, value)
     #print(sqlc)
     res = cur.execute(sqlc)
     conn.commit()
+
+T0 = datetime.datetime.now()
 
 conn = psycopg2.connect("dbname=monitor user=postgres")
 cur = conn.cursor()
 
 client = docker.from_env()
 container_list = client.containers.list()
+
+T1 = datetime.datetime.now()
+print((T1-T0).total_seconds())
+
+#Check for stopped containers
+sqlc = "select * from container;"
+cur.execute(sqlc)
+res = cur.fetchall()
+conn.commit()
+col_id = 0
+for i, e in enumerate(cur.description):
+    if e[0] == 'container_name':
+        col_id = i
+
+running_conts = [ r[col_id] for r in res ]
 for cont in container_list:
+    if cont.attrs['Name'] in running_conts:
+        running_conts.remove(cont.attrs['Name'])
+
+for r in running_conts:
+   update_item('container','running',"f", 'container_name', r)
+
+T2 = datetime.datetime.now()
+print((T2-T0).total_seconds())
+
+for ic, cont in enumerate(container_list):
     #Get container name
     container_name = cont.attrs['Name']
 
@@ -87,10 +115,10 @@ for cont in container_list:
 
         #Query for container_name, if it does not exist then insert it
         res = get_item('container', 'container_name', container_name)
-        if res and not res[1]:
-            update_item('container', 'project_id', "%d"%project_id, 'container_name', container_name, 'hubuser_id',"%d"%hubuser_id)
+        if res:
+            update_item('container','running',"t", 'container_name', container_name,)
         if not res:
-            insert_item('container', 'project_id, container_name, hubuser_id', "%d', '%s', '%d"%(project_id, container_name, hubuser_id))
+            insert_item('container', 'project_id, container_name, hubuser', "%d', '%s', '%d"%(project_id, container_name, hubuser_id))
             res = get_item('container', 'container_name', container_name)
         container_id = res[0]
 
@@ -98,30 +126,39 @@ for cont in container_list:
 
 
 
-
         #Get statistics from container
-        stat = cont.stats(stream=False)
+        T = datetime.datetime.now()
+        print((T-T0).total_seconds())
+        stat = cont.stats(decode=True, stream=False)
 
+        memoryusage = stat['memory_stats']['usage']
+        #percpu = array(stat['cpu_stats']['cpu_usage']['percpu_usage'])-array(stat['precpu_stats']['cpu_usage']['percpu_usage'])
+        #print("Usage per cpu for %s :"%container_name, percpu, percpu.mean())
+
+        # This is after https://github.com/moby/moby/blob/eb131c5383db8cac633919f82abad86c99bffbe5/cli/command/container/stats_helpers.go#L175
+        cpudelta = stat['cpu_stats']['cpu_usage']['total_usage']-stat['precpu_stats']['cpu_usage']['total_usage']
+        total_cpu = stat['cpu_stats']['cpu_usage']['total_usage']
+        precpudelta = stat['cpu_stats']['system_cpu_usage']-stat['precpu_stats']['system_cpu_usage']
+        cpuload = (cpudelta/precpudelta)*100*len(stat['cpu_stats']['cpu_usage']['percpu_usage'])
+        pids = stat['pids_stats']['current']
+
+        net_i = sum([ stat['networks'][interface]['rx_bytes'] for interface in stat['networks'].keys()])
+        net_o = sum([ stat['networks'][interface]['tx_bytes'] for interface in stat['networks'].keys()])
+
+
+        block_i = 0
+        block_o = 0
         try:
-           memoryusage = stat['memory_stats']['usage']
-           cpudelta = stat['cpu_stats']['cpu_usage']['total_usage']-stat['precpu_stats']['cpu_usage']['total_usage']
-           precpudelta = stat['cpu_stats']['system_cpu_usage']-stat['precpu_stats']['system_cpu_usage']
-           cpuload = (cpudelta/precpudelta)*100*len(stat['cpu_stats']['cpu_usage']['percpu_usage'])
-           pids = stat['pids_stats']['current']
-   
-           net_i = sum([ stat['networks'][interface]['rx_bytes'] for interface in stat['networks'].keys()])
-           net_o = sum([ stat['networks'][interface]['tx_bytes'] for interface in stat['networks'].keys()])
-   
-           block_i = stat['blkio_stats']['io_service_bytes_recursive'][5]['value']
-           block_o = stat['blkio_stats']['io_service_bytes_recursive'][6]['value']
-        except Exception as e:
-            print(e)
-            print(container_name)
-            print(stat)
+            block_i = stat['blkio_stats']['io_service_bytes_recursive'][5]['value']
+            block_o = stat['blkio_stats']['io_service_bytes_recursive'][6]['value']
+        except:
+            #print(stat)
+            print(stat['blkio_stats']['io_service_bytes_recursive'])
         
         #check first whether anything changed and then insert new numbers
         values = { 'cpuload' : cpuload,
                    'memoryusage' : memoryusage,
+                   'total_cpu' : total_cpu,
                    'pids' : pids,
                    'net_i' : net_i,
                    'net_o' : net_o,
@@ -130,6 +167,7 @@ for cont in container_list:
                  }
         table_names = { 'cpuload' : 'containerstats_cpuload',
                    'memoryusage' :   'containerstats_mem',
+                   'total_cpu' :   'containerstats_total_cpu',
                    'pids' : 'containerstats_pids',
                    'net_i' : 'containerstats_net_i',
                    'net_o' : 'containerstats_net_o',
@@ -138,6 +176,7 @@ for cont in container_list:
                  }
         threshold = { 'cpuload' : 10, # in %00
                    'memoryusage' : 0.01,
+                   'total_cpu' : 10000000000,
                    'pids' : 1,
                    'net_i' : 10**6,
                    'net_o' : 10**6,
@@ -146,6 +185,7 @@ for cont in container_list:
                   }
         s_type = { 'cpuload': 'c',
                    'memoryusage': 'a',
+                   'total_cpu': 'a',
                    'pids': 'a',
                    'net_i': 'a',
                    'net_o': 'a',
@@ -155,10 +195,17 @@ for cont in container_list:
 
         col_names = values.keys()
         for col_name in col_names:
-           if check_last_changed(table_names[col_name], container_id, col_name, values[col_name],\
+           if check_last_changed(table_names[col_name], hubuser_id, container_id, col_name, values[col_name],\
                   threshold[col_name], s_type[col_name]):
                1==1
-               insert_new_row(table_names[col_name], container_id, col_name, values[col_name])
+               insert_new_row(table_names[col_name], hubuser_id, container_id, col_name, values[col_name])
 
-        
 
+        if ic == 0:
+            total_cpu = stat['cpu_stats']['system_cpu_usage']
+            total_mem = stat['memory_stats']['limit']
+            insert_item('system','total_cpu, total_mem', "%d', '%d"%(total_cpu, total_mem) )
+
+
+TE = datetime.datetime.now()
+print((TE-T0).total_seconds())
