1d7c96e1778701a3d95add9cbdf64d3d8278e3cd
jegesm <jegesm@gmail.com>
2019-10-17 17:08:26 +0000

    fixed reporting nb server bugs

--- modules/notebook/scripts/02-mountuserhome.sh
+++ modules/notebook/scripts/02-mountuserhome.sh
@@ -3,7 +3,16 @@
 mkdir -p /tmp/.empty
 mount -t tmpfs tmpfs -o size=1K /home/
 mkdir -p /home/${NB_USER}
-mount -o bind /mnt/.volumes/home/${NB_USER} /home/${NB_USER}
+if [ -z "$REPORT_DIR" ]; then
+        mount -o bind /mnt/.volumes/home/${NB_USER} /home/${NB_USER}
+else
+        mkdir /mnt/report_temp/
+        mount -o bind /mnt/report_temp/ /home/$NB_USER
+        chmod a+wrx /home/$NB_USER
+        mkdir /home/report
+        mount -o bind /mnt/.volumes/report/$NB_USER /home/report/
+fi
+
 echo "Home mounted for ${NB_USER}"
 
 # hide volumes from the user
