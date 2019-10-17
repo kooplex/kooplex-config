b5ff5f7d47834bd13237009508b4ce0431e741e5
Steger, Jozsef <steger@complex.elte.hu>
2019-10-17 15:04:33 +0200

    fix env

--- modules/notebook/scripts/91-startsshagent.sh
+++ modules/notebook/scripts/91-startsshagent.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 if [ -z "$REPORT_TYPE" ] ; then
-        exec su $NB_USER -c "ssh-agent -a /tmp/$NB_USER ; export SSH_AUTH_SOCK=/tmp/$NB_USER"
+	if [ -z "$SSH_AUTH_SOCK" ] ; then
+		export SSH_AUTH_SOCK=/tmp/$NB_USER
+	fi
+        exec su $NB_USER -c "ssh-agent -a $SSH_AUTH_SOCK"
 	echo "ssh-agent started"
 else
 	echo "ssh-agent not started"
