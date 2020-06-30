6438736107f44766803765d300b59519d1a82a13
Steger, Jozsef <steger@complex.elte.hu>
2020-06-30 22:49:02 +0200

    seafile responds (memcached not configured yet!)

--- modules/seafile/configure.sh
+++ modules/seafile/configure.sh
@@ -4,9 +4,12 @@
 case $VERB in
   "build")
       echo "1. Configuring ${PREFIX}-${MODULE_NAME}..."
+#FIXME: all seafile persistent directories are handled together, split conf/data/log
 #      mkdir_svcconf
 #      mkdir_svclog
       mkdir_svcdata
+      CONFD=$MODDATA_DIR/seafile/seafile/conf/
+      _mkdir $CONFD
 
       sed -e s,##PREFIX##,$PREFIX, \
           -e s,##KUBE_MASTERNODE##,${KUBE_MASTERNODE}, \
@@ -17,11 +20,21 @@ case $VERB in
 	  -e s,##SEAFILE_ADMINPW##,$SEAFILEADMIN_PW, build/seafile.yaml-template \
           > $BUILDMOD_DIR/seafile.yaml
 
+      sed -e s,##REWRITEPROTO##,$REWRITEPROTO, \
+          -e s,##PREFIX##,$PREFIX, \
+          -e s,##FQDN##,$FQDN, \
+          -e s,##SEAFILEDB_PW##,$SEAFILEDB_PW, \
+          -e s,##URL_HYDRA##,$URL_HYDRA, \
+          -e s,##HYDRA_CLIENTID##,$HYDRA_SEAHUBCLIENTID, \
+          -e s,##DJANGO_SECRET_KEY##,$(echo $DJANGO_SECRET_KEY | sed -e 's/\$/$$/g'), \
+          -e s,##HYDRA_CLIENTSECRET##,$HYDRA_SEAHUBCLIENTSECRET, etc/seahub_settings.py-template \
+          > $CONFD/seahub_settings.py
+
   ;;
 
   "install")
       sed -e s,##PREFIX##,$PREFIX, etc/nginx-${MODULE_NAME}-template \
-      > $SERVICECONF_DIR/nginx/conf.d/sites-enabled/${MODULE_NAME}
+          > $SERVICECONF_DIR/nginx/conf.d/sites-enabled/${MODULE_NAME}
   ;;
 
   "start")
