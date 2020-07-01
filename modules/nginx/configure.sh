223e3c5031a340d6440b751587398f54e4fb340a
Steger, Jozsef <steger@complex.elte.hu>
2020-07-01 10:40:17 +0200

    split svcs and pods

--- modules/nginx/configure.sh
+++ modules/nginx/configure.sh
@@ -5,7 +5,7 @@ NGINX_KEYS=$FQDN_CERT
 
 case $VERB in
   "build")
-      echo "1. Configuring ${PREFIX}-${MODULE_NAME}..."
+      echo "1. Configuring ${PREFIX}-${MODULE_NAME}..." >&2
       mkdir_svcconf
       mkdir_svclog
       mkdir_svcdata
@@ -14,13 +14,17 @@ case $VERB in
 
       cp $KEYFILE $MODCONF_DIR/keys/${PREFIX}.key
       cp $CERTFILE $MODCONF_DIR/keys/${PREFIX}.crt
-      cp etc/custom* $MODDATA_DIR/
+      cp conf/custom* $MODDATA_DIR/
 
       sed -e s,##PREFIX##,$PREFIX, \
           -e s,##KUBE_MASTERNODE##,${KUBE_MASTERNODE}, \
           -e s,##MODULE_NAME##,$MODULE_NAME, \
-          -e s,##EXTERNALIP##,$EXTERNALIP, build/nginx.yaml-template \
-          > $BUILDMOD_DIR/nginx.yaml
+	  build/nginx-pods.yaml-template > $BUILDMOD_DIR/nginx-pods.yaml
+
+      sed -e s,##PREFIX##,$PREFIX, \
+          -e s,##MODULE_NAME##,$MODULE_NAME, \
+          -e s,##EXTERNALIP##,$EXTERNALIP, \
+	  build/nginx-svcs.yaml-template > $BUILDMOD_DIR/nginx-svcs.yaml
 
       sed -e s,##CERT##,${PREFIX}.crt, \
           -e s,##KEY##,${PREFIX}.key, \
@@ -34,22 +38,26 @@ case $VERB in
   ;;
 
   "start")
-       echo "Starting containers of ${PREFIX}-${MODULE_NAME}"
-       kubectl apply -f $BUILDMOD_DIR/nginx.yaml
+      echo "Starting services of ${PREFIX}-${MODULE_NAME}" >&2
+      kubectl apply -f $BUILDMOD_DIR/nginx-svcs.yaml
+      echo "Starting pods of ${PREFIX}-${MODULE_NAME}" >&2
+      kubectl apply -f $BUILDMOD_DIR/nginx-pods.yaml
   ;;
 
 
   "stop")
-      echo "Stopping containers of ${PREFIX}-${MODULE_NAME}"
-      kubectl delete -f $BUILDMOD_DIR/nginx.yaml
+      echo "Deleting pods of ${PREFIX}-${MODULE_NAME}" >&2
+      kubectl delete -f $BUILDMOD_DIR/nginx-pods.yaml
   ;;
 
   "remove")
+      echo "Deleting services of ${PREFIX}-${MODULE_NAME}" >&2
+      kubectl delete -f $BUILDMOD_DIR/nginx-svcs.yaml
   ;;
 
   "purge")
-      echo "Removing $BUILDMOD_DIR" 
-      rm -R -f $BUILDMOD_DIR
+      echo "Removing $BUILDMOD_DIR $MODDATA_DIR $MODCONF_DIR $MODLOG_DIR" >&2
+      rm -R -f $BUILDMOD_DIR $MODDATA_DIR $MODCONF_DIR $MODLOG_DIR
   ;;
 
 esac
