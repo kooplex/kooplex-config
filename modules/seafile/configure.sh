0ab9c8b9524e738866154eb5c4d4ccab08a76545
Steger, Jozsef <steger.jozsef@gmail.com>
2020-10-21 14:40:57 +0000

    seafile

--- modules/seafile/configure.sh
+++ modules/seafile/configure.sh
@@ -8,11 +8,9 @@ case $VERB in
 #      mkdir_svcconf
 #      mkdir_svclog
       mkdir_svcdata
-      CONFD=$MODDATA_DIR/seafile/seafile/conf/
-      _mkdir $CONFD
 
       sed -e s,##PREFIX##,$PREFIX, \
-          -e s,##KUBE_MASTERNODE##,${KUBE_MASTERNODE}, \
+          -e s,##SERVICENODE##,${SERVICE_NODE}, \
           -e s,##MODULE_NAME##,$MODULE_NAME, \
           -e s,##FQDN##,$FQDN, \
 	  -e s,##SEAFILE_MYSQL_ROOTPW##,$SEAFILEDB_PW, \
@@ -24,21 +22,30 @@ case $VERB in
           -e s,##MODULE_NAME##,$MODULE_NAME, \
 	  build/seafile-svcs.yaml-template > $BUILDMOD_DIR/seafile-svcs.yaml
 
-      sed -e s,##REWRITEPROTO##,$REWRITEPROTO, \
-          -e s,##PREFIX##,$PREFIX, \
-          -e s,##FQDN##,$FQDN, \
-          -e s,##SEAFILEDB_PW##,$SEAFILEDB_PW, \
-          -e s,##URL_HYDRA##,$URL_HYDRA, \
-          -e s,##HYDRA_CLIENTID##,$HYDRA_SEAHUBCLIENTID, \
-          -e s,##DJANGO_SECRET_KEY##,$(echo $DJANGO_SECRET_KEY | sed -e 's/\$/$$/g'), \
-          -e s,##HYDRA_CLIENTSECRET##,$HYDRA_SEAHUBCLIENTSECRET, \
-	  conf/seahub_settings.py-template > $CONFD/seahub_settings.py
+  ;;
+
+  "init")
+      echo "Initiaizing services of ${PREFIX}-${MODULE_NAME}" >&2
+      STATE=$(kubectl get pods | awk "/^${PREFIX}-${MODULE_NAME}\s/ {print \$3}")
+      if [ "$STATE" = "Running" ] ; then
+          register_module_in_hydra
+          sed -e s,##REWRITEPROTO##,$REWRITEPROTO, \
+              -e s,##PREFIX##,$PREFIX, \
+              -e s,##FQDN##,$FQDN, \
+              -e s,##SEAFILEDB_PW##,$SEAFILEDB_PW, \
+              -e s,##URL_HYDRA##,$URL_HYDRA, \
+              -e s,##HYDRA_CLIENTID##,$HYDRA_SEAHUBCLIENTID, \
+              -e s,##DJANGO_SECRET_KEY##,$(echo $DJANGO_SECRET_KEY | sed -e 's/\$/$$/g'), \
+              -e s,##HYDRA_CLIENTSECRET##,$SECRET, \
+	  conf/seahub_settings.py-template > $BUILDMOD_DIR/seahub_settings.py
+	  kubectl cp $BUILDMOD_DIR/seahub_settings.py helper:/data/seafile/seafile/seafile/conf/seahub_settings.py
+      else
+          echo "Pod for $MODULE_NAME is not running" >&2
+      fi
   ;;
 
   "install")
-      sed -e s,##PREFIX##,$PREFIX, \
-          conf/nginx-${MODULE_NAME}-template > $SERVICECONF_DIR/nginx/conf.d/sites-enabled/${MODULE_NAME}
-      restart_nginx
+      register_module_in_nginx
   ;;
 
   "start")
