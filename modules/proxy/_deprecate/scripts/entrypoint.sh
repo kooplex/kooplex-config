1d189f245cc6e3de9e413689ab911d07947cc20f
Steger, Jozsef <steger@complex.elte.hu>
2021-03-18 11:32:56 +0100

    clean deprecated code

--- modules/proxy/_deprecate/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Executing proxy at $CHP_PUBLICIP, $CHP_ADMINIP"
-
-cd /srv/configurable-http-proxy
-exec configurable-http-proxy \
-    --ip ${PUBLICIP} \
-    --api-ip ${ADMINIP}
