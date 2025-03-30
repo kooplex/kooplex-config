9bcbbefd2396e84677f63a00774900d48d72afab
fonok <david.visontai@complex.elte.hu>
2025-03-30 20:31:42 +0000

    build script update

--- build_from_jsonnet.sh
+++ build_from_jsonnet.sh
@@ -1,23 +1,42 @@
-BUILDDIR=raw
-MANIFEST=manifest
+#!/bin/bash
+
+MODULE=$1
+mkdir -p _build
+BUILDDIR="_build/raw_${MODULE}"
+MANIFEST="_build/manifest_${MODULE}"
+
+if [[ -z "${MODULE}" ]]; then
+	echo ERROR: MODULE is not set
+	echo "Usage: build_from_jsonnet.sh <module>"
+	exit
+else
+	echo "Build ${MODULE} module"
+fi
+
+if [[ -z "${KOOPLEX_REPO}" ]]; then
+	echo "Using local directory ($PWD) to populate config files"
+	KOOPLEX_REPO=$PWD
+else
+	echo "Using ${KOOPLEX_REPO} to populate config files"
+fi
 
 mkdir -p ${BUILDDIR} ${MANIFEST} 
 
 cd ${BUILDDIR}
 
-for f in `ls ../*.jsonnet`
+for f in `ls ${KOOPLEX_REPO}/${MODULE}/*.jsonnet`
 do
 	sf=$(echo $f | tr "." " " | awk '{print $1}')
-	jsonnet -m . $f; 
+	jsonnet -m . $f ; 
 done
 
 for f in `ls *-raw`
 do
 	sf=$(echo $f | tr "-" " " | awk '{print $1}')
-	cat ${f} | gojsontoyaml > ../${MANIFEST}/${sf}
+	cat ${f} | gojsontoyaml > ${KOOPLEX_REPO}/${MANIFEST}/${sf}
 done
 
-cd ..
+#cd _build
 
 
 # export r=`jsonnet -e "std.manifestYamlStream(
